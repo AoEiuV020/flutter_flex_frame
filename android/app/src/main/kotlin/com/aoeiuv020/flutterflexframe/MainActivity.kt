5a4011f6101edf0a30b9ee34c435e93d84919df7
AoEiuV020 <aoeiuv020@gmail.com>
2025-01-05 17:41:11 +0800

    create app

---
+++ android/app/src/main/kotlin/com/aoeiuv020/flutterflexframe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.aoeiuv020.flutterflexframe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- script/plugin_create.sh
+++ script/plugin_create.sh
@@ -2,28 +2,29 @@
 . "$(dirname $0)/env.sh"
 
 name=$1
-# name 传入了包名就在apps创建包，否则创建修改根项目，
+# name 传入了包名就在packages创建包，否则创建修改根项目，
 if [ -z "$name" ] || [ "$name" = "." ]; then
     cd "$ROOT"
     rm -f pubspec.yaml README.md
     mv .gitignore .gitignore.bak
     # 删除各平台代码
     rm -rf windows/ macos/ linux/ ios/ android/ web/
-    flutter create --org "$organization" --project-name "$app_name" --template=app . "${@:2}"
+    flutter create --org "$organization" --project-name "$project_name" --template=plugin . "${@:2}"
     dart pub add dev:melos
     cat .gitignore.bak >>.gitignore
     rm -f .gitignore.bak
 else
     # 检查并创建目录
-    if [ ! -d "$apps_dir" ]; then
-        echo "Directory $apps_dir does not exist. Creating..."
-        mkdir -p "$apps_dir"
+    if [ ! -d "$packages_dir" ]; then
+        echo "Directory $packages_dir does not exist. Creating..."
+        mkdir -p "$packages_dir"
     fi
-    cd "$apps_dir"
+    cd "$packages_dir"
     # 删除各平台代码
     rm -rf windows/ macos/ linux/ ios/ android/ web/
-    flutter create --org "$organization" --template=app "$name" "${@:2}"
+    flutter create --org "$organization" --template=plugin "$name" "${@:2}"
     cd "$name"
+    cp "$ROOT/LICENSE" .
 fi
 echo 'include: package:flutter_lints/flutter.yaml' >analysis_options.yaml
 cat "$script_dir"/analyzer_custom.yaml >>analysis_options.yaml
